227317aa1f74b002af9d5e885170b61350ad6642
jangs7078 <bjang1@stanford.edu>
2015-04-23 11:21:43 -0700

    first push

---
+++ Deal/SwiftHeader.h
@@ -0,0 +1,17 @@
+//
+//  SwiftHeader.h
+//  Deal
+//
+//  Created by Ki Suk Jang on 4/23/15.
+//  Copyright (c) 2015 Ki Suk Jang. All rights reserved.
+//
+
+#ifndef Deal_SwiftHeader_h
+#define Deal_SwiftHeader_h
+
+#import <AWSCore/AWSCore.h>
+#import <AWSDynamoDB/AWSDynamoDB.h>
+#import <AWSDynamoDB/AWSDynamoDBModel.h>
+#import <AWSCognito/AWSCognito.h>
+
+#endif
