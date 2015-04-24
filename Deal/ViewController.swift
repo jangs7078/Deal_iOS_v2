15b1f1b4f04c2ac96925d6adbb40631ca2d6ad6d
jangs7078 <bjang1@stanford.edu>
2015-04-23 22:33:15 -0700

    profile icon added

--- Deal/ViewController.swift
+++ Deal/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var my_icon: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Round my_icon
+        my_icon.layer.cornerRadius = my_icon.frame.size.height / 2;
+        my_icon.clipsToBounds = true
+        my_icon.layer.borderWidth = 0.5
+        my_icon.layer.borderColor = UIColor.blackColor().CGColor
+        
+        // Add motion effect on my_icon
+        my_icon.addMotionEffect(getMotionEffectGroup())
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getMotionEffectGroup () ->UIMotionEffect {
+        // Set vertical effect
+        let verticalMotionEffect = UIInterpolatingMotionEffect(keyPath: "center.y",
+            type: .TiltAlongVerticalAxis)
+        verticalMotionEffect.minimumRelativeValue = -10
+        verticalMotionEffect.maximumRelativeValue = 10
+        
+        // Set horizontal effect
+        let horizontalMotionEffect = UIInterpolatingMotionEffect(keyPath: "center.x",
+            type: .TiltAlongHorizontalAxis)
+        horizontalMotionEffect.minimumRelativeValue = -10
+        horizontalMotionEffect.maximumRelativeValue = 10
+        
+        // Create group to combine both
+        let group = UIMotionEffectGroup()
+        group.motionEffects = [horizontalMotionEffect, verticalMotionEffect]
+        
+        return group
+    }
 
 }
 
